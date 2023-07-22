fca7d23433165ebf654af0bfbd9ba1a30a897046
wonk93 <danielsalasrodriguez93@gmail.com>
2023-07-22 10:37:45 +0200

    intento de api

--- src/pages/OnlineRecipeSearch.jsx
+++ src/pages/OnlineRecipeSearch.jsx
@@ -1,8 +1,15 @@
+import axios from "axios";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 function OnlineRecipeSearch() {
     const { name } = useParams();
-
+console.log(name)
+const [display, setDisplay]= useState([])
+axios.get('www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata').then((response)=>{
+    setDisplay(response.data).catch((error)=> console.error(error))
+    console.log(display);
+})
     return (
         <article>
             <h1>Receta online de {name}</h1>
